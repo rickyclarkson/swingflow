12823bb2f202bec494243e9e2fd586ef7166fd00
Ricky Clarkson <ricky.clarkson@gmail.com>
2012-07-09 22:56:33 -0300

    More style.

--- src/main/java/com/github/rickyclarkson/swingflow/DetailsButton.java
+++ src/main/java/com/github/rickyclarkson/swingflow/DetailsButton.java
@@ -1,21 +1,29 @@
 package com.github.rickyclarkson.swingflow;
 
-import net.miginfocom.swing.MigLayout;
-
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JWindow;
 import javax.swing.border.Border;
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.LayoutManager;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import static com.github.rickyclarkson.swingflow.Option._None;
 import static com.github.rickyclarkson.swingflow.Option._Some;
+import static com.github.rickyclarkson.swingflow.Windows.close;
 
 public class DetailsButton extends JButton {
     private Option<JTextArea> popup = _None();
@@ -29,14 +37,18 @@ public class DetailsButton extends JButton {
                     @Override
                     public void _case(Option.Some<JTextArea> someTextArea) {
                         JWindow window = new JWindow();
+
                         final Border border = BorderFactory.createLineBorder(Color.black);
                         someTextArea.t.setBorder(border);
-                        final JPanel northPanel = new JPanel(new MigLayout());
-                        northPanel.add(new JLabel("Details"));
-                        northPanel.add(new JButton("X"), "");
+                        final JPanel northPanel = gradientPanel(new BorderLayout());
+                        northPanel.add(new JLabel("Details"), BorderLayout.WEST);
+                        northPanel.add(closeButton(close(window)), BorderLayout.EAST);
+                        window.add(northPanel, BorderLayout.NORTH);
                         window.add(someTextArea.t);
+                        window.setMinimumSize(new Dimension(200, 100));
                         window.pack();
-                        window.setBackground(Color.yellow.darker());
+                        someTextArea.t.setFocusable(true);
+                        someTextArea.t.setEditable(true);
                         Rectangle bounds = window.getGraphicsConfiguration().getBounds();
                         int x = getLocationOnScreen().x;
                         int y = getLocationOnScreen().y + getSize().height;
@@ -63,6 +75,33 @@ public class DetailsButton extends JButton {
                 });
             }
         });
+    }
+
+    private JPanel gradientPanel(LayoutManager layout) {
+        return new JPanel(layout) {
+            public void paintComponent(Graphics graphics) {
+                super.paintComponent(graphics);
+                Graphics2D g2d = (Graphics2D)graphics.create();
+                g2d.setPaint(new GradientPaint(0, 0, averageColor(Color.gray, Color.cyan), 0, getHeight(), Color.white));
+                g2d.fillRect(0, 0, getWidth(), getHeight());
+                g2d.dispose();
+            }
+        };
+    }
+
+    private Color averageColor(Color one, Color two) {
+        return new Color((one.getRed() + two.getRed()) / 2, (one.getGreen() + two.getGreen()) / 2, (one.getBlue() + two.getBlue()) / 2);
+    }
+
+    private JComponent closeButton(final ActionListener listener) {
+        final JLabel button = new JLabel(" X ");
+        button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                listener.actionPerformed(null);
+            }
+        });
+        return button;
     }
 
     @EDT
--- src/main/java/com/github/rickyclarkson/swingflow/Windows.java
+++ src/main/java/com/github/rickyclarkson/swingflow/Windows.java
@@ -1,6 +1,9 @@
 package com.github.rickyclarkson.swingflow;
 
+import javax.swing.JWindow;
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Windows {
     public static <T> Option<T> findAncestor(Component component, Class<T> ancestorClass) {
@@ -12,5 +15,14 @@ public class Windows {
         }
 
         return Option._None();
+    }
+
+    public static ActionListener close(final JWindow window) {
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                window.setVisible(false);
+            }
+        };
     }
 }
